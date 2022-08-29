9f6d39725169bc734719fb22609b3c3f8cc2792b
Fernando-Leon <leon.monroy.fernando@gmail.com>
2022-08-29 10:29:12 -0500

    cambio 1

--- JS/main.js
+++ JS/main.js
@@ -1,32 +1,41 @@
-var token = "a66ff00a1b88f02c41015ef4e8f2a651f1d6ed74ad1aa2f792f982fbce221da8";
+var token = "a66ff00a1b88f02c41015ef4e8f2a651f1d6ed74ad1aa2f792f982fbce221da8";//Token API Banxico 
 var seriesBilletes = ["SM1471", "SM1472", "SM1478", "SM1479", "SM1480", "SM1481", "SM1482"];
 var seriesMonedas = ["SM9", "SM10", "SM11", "SM12", "SM13", "SM14", "SM15", "SM16", "SM17", "SM18", "SM19"];
-
+var urlMonedas = ["5centavos.png", "10centavos.png", "20centavos.png", "50centavos.png", "1peso.png", "2pesos.png", "5pesos.png", "10pesos.png", "20pesos.png", "50pesos.png", "100pesos.png"];
+var urlBilletes = ["billete10.jpg", "billete20.png", "billete50.png", "billete100.png", "billete200.png", "billete500.png", "billete1000.png"]
 const getValues = async () => {
-    const values  = []
-    for(let i = 0; i < seriesMonedas.length; i++){
-        let requestMondas = await axios(`https://www.banxico.org.mx/SieAPIRest/service/v1/series/${seriesMonedas[i]}/datos/oportuno?token=${token}`);
-        values.push(requestMondas.data);
+    let arrM = [];
+    let arrB = [];
+    for(let i = 0; i < seriesMonedas.length; i++){//Get Info Monedas
+        let request = await axios(`https://www.banxico.org.mx/SieAPIRest/service/v1/series/${seriesMonedas[i]}/datos/oportuno?token=${token}`);
+        arrM.push(request.data.bmx.series);
     }
-    for(let i = 0; i < seriesBilletes.length; i++) {
-        let requestBilletes = await axios(`https://www.banxico.org.mx/SieAPIRest/service/v1/series/${seriesBilletes[i]}/datos/oportuno?token=${token}`);
-        values.push(requestBilletes.data);
+    for(let i = 0; i < seriesBilletes.length; i++){//Get Info Billtes
+        let request = await axios(`https://www.banxico.org.mx/SieAPIRest/service/v1/series/${seriesBilletes[i]}/datos/oportuno?token=${token}`);
+        arrB.push(request.data.bmx.series);
     }
-    console.log(values);
-    setValues(values);
+    setValues(arrB, arrM);
 }
-
-getValues(); 
-
-const setValues = (vals) => {
+const setValues = (arrBilles, arrMonedas) => {
     let container = document.getElementById("container-values");
-    for(let i = 0; i < vals.length; i++) {
-        let codeHTML = `<div id="item">
-                            <div id="title">${vals[i].bmx.series[0].titulo}</div>
-                            <div id="date">${vals[i].bmx.series[0].datos[0].fecha}</div>
-                            <div id"stock">${vals[i].bmx.series[0].datos[0].dato}</div>
-                            <div id="cont-image"><img src="../IMGS/resourse.jpg"></div> 
-                        </div>`;
-        container.innerHTML += codeHTML;
+    let codeHTML = "";
+    codeHTML += `<div class="title-items">Monedas</div>`
+    for(let i = 0; i < arrMonedas.length; i++){//Push Monedas
+        codeHTML += `<div class="item">
+                        <div class="title">${arrMonedas[i][0].titulo}.</div>
+                        <div class="date">Fecha de corte: ${arrMonedas[i][0].datos[0].fecha}.</div>
+                        <div class="stock">Unidades en circulación: ${arrMonedas[i][0].datos[0].dato} millones.</div>
+                        <div class="cont-image"><img src="../IMGS/${urlMonedas[i]}"></div> 
+                     </div>`
     }
+    codeHTML += `<div class="title-items">Billetes</div>`
+    for(let i = 0; i < arrBilles.length; i++){//Push Billetes
+        codeHTML += `<div class="item">
+                        <div class="title">${arrBilles[i][0].titulo}.</div>
+                        <div class="date">Fecha de corte: ${arrBilles[i][0].datos[0].fecha}.</div>
+                        <div class="stock">Unidades en circulación: ${arrBilles[i][0].datos[0].dato} millones.</div>
+                        <div class="cont-image"><img src="../IMGS/${urlBilletes[i]}"></div> 
+                     </div>`
+    }
+    container.innerHTML = codeHTML;
 }
